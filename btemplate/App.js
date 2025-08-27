81884e3b778d85c5012247f710c38163d4ccb390
AngeloDev <angelo.dev.santos@gmail.com>
2025-08-27 00:26:01 -0300

    Aula 14 aprendemos a passar props pro componente e criar componentes a partir da classe component

--- btemplate/App.js
+++ btemplate/App.js
@@ -1,5 +1,5 @@
 import { Component } from "react";
-import { View,Text } from "react-native";
+import { View,Text ,Image} from "react-native";
 class App extends Component{
 
   render(){
@@ -13,8 +13,29 @@ class App extends Component{
             fontSize:30
           }
         }>este texto ficara vermelho e grande 'lembrar de sempre usar aspas so pra strings'</Text>
+      <Tigre largura={300} altura={300} message={'Trigri bolado'}/>
+      <Text>Quase ia me esquecendo da imagem que aprendi a adicionar aqui tbm </Text>
       </View>
+
     )
   }
 }
 export default App
+
+class Tigre extends Component{
+  
+  render(){
+    let link = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMwz6gMfE5tld-6S4pYfs60t81RZQ7lJtaUI69S98Sz55XlMBIild_d7IrnjHjKsPzYFg&usqp=CAU'
+    return(
+      <View>
+        <Image
+        source={{uri:link}}
+        style={{width:this.props.largura,height:this.props.altura}}
+      />  
+      <Text>{this.props.message}</Text>
+
+
+      </View>
+    )
+  }
+}
