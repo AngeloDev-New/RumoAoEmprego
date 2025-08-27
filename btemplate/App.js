d19a3f706c726ca9e08b92ad6a17445c143f8d5d
AngeloDev <angelo.dev.santos@gmail.com>
2025-08-27 00:50:58 -0300

    Aula15 aprendemos um pouco sobre state ....nao devo esquece que constructor se escreve com c minusculo

--- btemplate/App.js
+++ btemplate/App.js
@@ -1,41 +1,32 @@
 import { Component } from "react";
-import { View,Text ,Image} from "react-native";
+import { View,Text ,Button} from "react-native";
 class App extends Component{
-
-  render(){
-    return(
-      <View>
-        <Text>Ola mundo</Text>
-        <Text>Entendendo um poucoi sobre propriedades</Text>
-        <Text style={
-          {
-            color:'#ff0000',
-            fontSize:30
-          }
-        }>este texto ficara vermelho e grande 'lembrar de sempre usar aspas so pra strings'</Text>
-      <Tigre largura={300} altura={300} message={'Trigri bolado'}/>
-      <Text>Quase ia me esquecendo da imagem que aprendi a adicionar aqui tbm </Text>
-      </View>
-
+  constructor(props){
+    super(props);
+    this.state={
+      nome:''
+    };
+    this.entrar = this.entrar.bind(this);
+  }
+  entrar(nome){
+    this.setState(
+      {
+        nome:nome
+      }
     )
+  }
+  render(){
+    return (
+      <View style={{margin:50}}>
+        <Button title="entrar" onPress={()=>this.entrar('Angelo')}/>
+        <Text style={{
+          fontSize:23,
+          color:'red',
+          textAlign:'center'
+        }}>{this.state.nome}</Text>
+      </View>
+    );
   }
 }
 export default App
 
-class Tigre extends Component{
-  
-  render(){
-    let link = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMwz6gMfE5tld-6S4pYfs60t81RZQ7lJtaUI69S98Sz55XlMBIild_d7IrnjHjKsPzYFg&usqp=CAU'
-    return(
-      <View>
-        <Image
-        source={{uri:link}}
-        style={{width:this.props.largura,height:this.props.altura}}
-      />  
-      <Text>{this.props.message}</Text>
-
-
-      </View>
-    )
-  }
-}
