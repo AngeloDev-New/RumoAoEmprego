b66c32dd7feb2c84c01857244062f683651a7948
AngeloDev <angelo.dev.santos@gmail.com>
2025-08-27 00:06:23 -0300

    aula 13 falamos um pouco de props (propriedades) dos componentes

--- btemplate/App.js
+++ btemplate/App.js
@@ -1,10 +1,18 @@
 import { Component } from "react";
 import { View,Text } from "react-native";
 class App extends Component{
+
   render(){
     return(
       <View>
         <Text>Ola mundo</Text>
+        <Text>Entendendo um poucoi sobre propriedades</Text>
+        <Text style={
+          {
+            color:'#ff0000',
+            fontSize:30
+          }
+        }>este texto ficara vermelho e grande 'lembrar de sempre usar aspas so pra strings'</Text>
       </View>
     )
   }
