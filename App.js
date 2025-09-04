d81cc69f956aaf9d89c883615d5633a7df972374
AngeloDev <angelo.dev.santos@gmail.com>
2025-09-04 00:20:22 -0300

    Documentando pra lembrar dia 04-set

--- App.js
+++ App.js
@@ -1,15 +1,23 @@
-import React ,{Component} from 'react';
+// classe responsavel por criar componentes
+import {Component} from 'react';
+// componentes padrao usados no app
 import { View,Text,StyleSheet,TextInput } from 'react-native';
-
+// criando componente prprio que herda do componente inportado do proprio react
 class App extends Component{
+    // criando construtor pra tabalhar com states
     constructor(props){
+        // passando as props pra classe pai
         super(props);
+        // definindo variaveis do state
         this.state = {
+            // variavel nome definida
             nome:'Angelo'
         };
+        // vinculando metodo a classe burocrassia do js ...da pra usar arrow
         this.pagaNome = this.pegaNome.bind(this);
 
     }
+    // definindo funca
     pegaNome(texto){
         if (texto.length > 0){
             this.setState({nome:'Bem vindo: '+texto});
@@ -18,8 +26,10 @@ class App extends Component{
             this.setState({nome:''})
         }
     }
+    // funcao principal de renderizacao
     render(){
         return (
+            
             <View style={styles.container}>
                 <TextInput
                     style={styles.input}
