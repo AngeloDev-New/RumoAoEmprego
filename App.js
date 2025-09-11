7ce5a3f8c45f48f3cf9d1b9162827e01c673f4e9
AngeloDev <angelo.dev.santos@gmail.com>
2025-09-11 20:47:34 -0300

    Bibliotecas nativas

---
+++ App.js
@@ -0,0 +1,18 @@
+import {
+  NativeModules,
+  View,
+  Text,
+} from 'react-native';
+import {Component} from 'react';
+
+class app extends Component{
+  render(){
+    return(
+      <View>
+        <Text>
+          Ola
+        </Text>
+      </View>
+    )
+  } 
+}
