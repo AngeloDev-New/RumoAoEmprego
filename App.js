c77bcbd5d27ecd495f77cda32eb5708ba4bb6692
AngeloDev <angelo.dev.santos@gmail.com>
2025-09-12 15:37:53 -0300

    Aula 26 aprendendo um pouco sobre FlatList

--- App.js
+++ App.js
@@ -1,18 +1,62 @@
+import React , {Component} from 'react';
 import {
-  NativeModules,
-  View,
+  View,//ds
   Text,
-} from 'react-native';
-import {Component} from 'react';
+  StyleSheet,
+  FlatList
+} from 'react-native'
 
-class app extends Component{
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      feed:[
+        {id:1,nome:'Eduardo',idade:19,email:'eduardo@dominio.com'},
+        {id:2,nome:'Camilly',idade:25,email:'camilly@dominio.com'},
+        {id:3,nome:'Angelo',idade:18,email:'angelo@dominio.com'},
+        {id:4,nome:'Frederico',idade:16,email:'frederico@dominio.com'},
+        {id:5,nome:'Robsson',idade:24,email:'robsson@dominio.com'},
+        {id:6,nome:'Fernanda',idade:62,email:'fernanda@dominio.com'},
+        {id:7,nome:'Gabriel',idade:17,email:'gabriel@dominio.com'},
+      ]
+    }
+  }
   render(){
     return(
-      <View>
-        <Text>
-          Ola
-        </Text>
+      <View Style={styles.container}>
+          <FlatList 
+            data={this.state.feed}
+            keyExtractor={(item)=> item.id} 
+            renderItem={({item})=> <Pessoa data={item}/>}/>
       </View>
-    )
-  } 
+    );
+  }
+}
+styles = StyleSheet.create({
+  container:{
+    flex:1,
+  },  
+  feed:{
+    color:'#fff',
+    fontSize:20,
+  },
+  feedArea:{
+    backgroundColor : '#222',
+    height:200,
+    marginBottom:15
+  }
+});
+
+export default App;
+
+class Pessoa extends Component{
+  render(){
+    return(
+      <View style={styles.feedArea}>
+        <Text style={styles.feed}>Nome:{this.props.data.nome}</Text>
+        <Text style={styles.feed}>idade:{this.props.data.idade}</Text>
+        <Text style={styles.feed}>Email:{this.props.data.email}</Text>
+      </View>
+    );
+  }
 }
