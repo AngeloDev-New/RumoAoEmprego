180723ed327de879f73a70670db901ee16f69b76
AngeloDev <angelo.dev.santos@gmail.com>
2025-09-15 15:40:48 -0300

    Subindo desafio da aula 31

--- App.js
+++ App.js
@@ -1,43 +1,104 @@
 import React , {Component} from 'react';
 import {
-  View,//ds
+  View,
   Text,
   StyleSheet,
-  FlatList
+  TextInput,
+  Button,
+  Alert
 } from 'react-native'
-import Pessoa from './src/Pessoa'
-class App extends Component{
+import {Picker} from '@react-native-picker/picker';
+import { Switch } from 'react-native-switch';
+import Slider from '@react-native-community/slider';
+export default class App extends Component{
   constructor(props){
     super(props);
     this.state = {
-      feed:[
-        {id:1,nome:'Eduardo',idade:19,email:'eduardo@dominio.com'},
-        {id:2,nome:'Camilly',idade:25,email:'camilly@dominio.com'},
-        {id:3,nome:'Angelo',idade:18,email:'angelo@dominio.com'},
-        {id:4,nome:'Frederico',idade:16,email:'frederico@dominio.com'},
-        {id:5,nome:'Robsson',idade:24,email:'robsson@dominio.com'},
-        {id:6,nome:'Fernanda',idade:62,email:'fernanda@dominio.com'},
-        {id:7,nome:'Gabriel',idade:17,email:'gabriel@dominio.com'},
-      ]
+      name:'',
+      idade:'',
+      sexo:0,
+      generos:[
+        {key:0,genero:'Selecione o Genero'},
+        {key:1,genero:'Masculino'},
+        {key:2,genero:'Feminino'},
+        {key:3,genero:'Hermafrodita'},
+      ],
+      limitValue:5000,
+      isStudent:true,
     }
   }
   render(){
+    let generos = this.state.generos.map((v,k)=>{
+      return(
+        <Picker.Item key={v.key} value={v.key} label = {v.genero}/>
+      );
+    })
     return(
-      <View Style={styles.container}>
-          <FlatList 
-            data={this.state.feed}
-            keyExtractor={(item)=> item.id} 
-            renderItem={({item})=> <Pessoa data={item}/>}/>
+      <View style={styles.container}>
+        <View style={styles.subcontainer}>
+
+        <Text style={styles.text}>Banco React</Text>
+        <Text style={styles.text}>Nome:</Text><TextInput
+          onChangeText={input=>this.setState({name:input})}
+         />
+        <Text style={styles.text}>Idade:</Text><TextInput
+          onChangeText={input=>this.setState({idade:input})}
+         />
+         <Picker
+          selectedValue={this.state.sexo}
+          onValueChange={(itenvalue,itenindex)=>this.setState({sexo:itenvalue})}
+         >
+          {generos}
+         </Picker>
+         <Slider
+          minimumValue={0}
+          maximumValue={5000}
+          onValueChange={(v)=>this.setState({limitValue:v})}
+         />
+         <Text style={styles.Text}>Valor:{this.state.limitValue.toFixed(2)}</Text>
+         <Text style={styles.text}>{this.state.isStudent ? 'Sou ':'Nao sou '}estudante</Text>
+         <Switch
+          value={this.state.isStudent}
+          onValueChange={(v)=>this.setState({isStudent:v})}
+         />
+          <Button
+            title='Confirmar'
+            onPress={()=>{
+              if(!(
+                (!this.state.sexo) ||   
+                (!this.state.name) ||   
+                (!this.state.idade))  
+              ){
+                Alert.alert(`Ola ${this.state.name} do genero ${this.state.generos[this.state.sexo].genero} voce tem ${this.state.idade} anos. Voce realmente deseja adquirir um limite de R\$${this.state.limitValue.toFixed(2)} no seu cartao de credito?`)
+              }else{
+                Alert.alert('Preencha todos os campos')
+              }
+            }}
+          />
+        </View>
+
       </View>
+
     );
   }
+
 }
-styles = StyleSheet.create({
+const colors = {
+  a:'#32556B',
+  b:'#3A5D73',
+  c:'#488FC8',
+  d:'#6CCB17',
+  e:'#FFC605'
+};
+const styles = StyleSheet.create({
   container:{
     flex:1,
-  },  
+    backgroundColor:colors.a
+  },
+  text:{
+    backgroundColor:colors.d,
+    color:colors.a,
+    fontSize:25
 
+  }
 });
-
-export default App;
-
